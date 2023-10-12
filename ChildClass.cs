cb8a7c37836b6d9998e56fd35ce1bb3bb7ccec30
Chitrang <Chitr@LENOVO>
2023-10-12 09:37:14 +0530

    Add project files.

---
+++ ChildClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Basics {
+    internal class ChildClass : Program{
+        public static void Main(string[] args) {
+            ChildClass c = new ChildClass();
+            c.ProtectedFunction();
+            c.InternalFunctin();
+            c.InternalProtectedFunction();
+            c.PublicFunction();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program {
+    public static void Main() {
+        Program p = new Program();
+
+        p.PrivateFunction();
+        p.ProtectedFunction();
+        p.InternalFunctin();
+        p.InternalProtectedFunction();
+        p.PublicFunction();
+    }
+
+    private void PrivateFunction() {
+        Console.WriteLine($"Private Function Called");
+    }
+
+    protected void ProtectedFunction() {
+        Console.WriteLine("Protected Function Called");
+    }
+
+    internal void InternalFunctin() {
+        Console.WriteLine("Internal Function Called");
+    }
+
+    internal protected void InternalProtectedFunction() {
+        Console.WriteLine("Internal Protected Called");
+    }
+
+    public void PublicFunction() {
+        Console.WriteLine("Public Function");
+    }
+}
